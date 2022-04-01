7cdb5fd5139827427aef5acd4a31f2ec330bf8dc
gating <674964222@qq.com>
2022-04-01 23:49:34 +0800

    fix: 一言方法丢失

--- wechat/reply.js
+++ wechat/reply.js
@@ -12,7 +12,8 @@ const {
   replyLazy,
   replyFlatterer,
   replyChickenSoup,
-  replyRandomFace
+  replyRandomFace,
+  replyLoveTalk
 } = require('./bot')
 const help = '亲爱的，欢迎关注磨蹭的小时光'
 const urlReg = /(((ht|f)tps?):\/\/)?[\w-]+(\.[\w-]+)+([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?/
